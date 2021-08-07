2fa4c833c59db2e05ed6324c12d9907e3c946c2b
unknown <you@example.com>
2021-08-07 21:27:09 +0530

    hrllo

---
+++ index.js
@@ -0,0 +1,112 @@
+//javascript window
+
+// document.getElementById("window").innerHTML = " show the Height of window" + window.innerHeight + "px <br>" +
+
+//                                             "show the width of window" + window.innerWidth + "px"
+
+// window open
+
+// var window = window.open("https://www.google.com/")
+// document.write(window)
+
+// window close()
+
+// var window = window.close()
+
+// document.write(winodw)
+
+// window moveTo 
+
+// var window = window.moveTo(20,20)
+
+// document.write(winodw)
+
+// function checkOrigin(){
+//    console.log(window.moveTo(0,0))
+// }
+// checkOrigin()
+
+// document.write(window.resizeTo(200,400))
+
+
+// function makeScreenQuarter(){
+//     window.resizeTo(
+//         window.screen.availWidth/2,
+//         window.screen.availHeight/2
+//     )
+// }
+// console.log(makeScreenQuarter())
+
+
+// screen width , height, availWidth, availHeight, colorDepth, pixelDepth 
+
+
+// document.getElementById("screen").innerHTML="Screen's height" + screen.pixelDepth
+
+
+//document.getElementById("common").innerHTML = "page url : " + window.location.href
+
+// document.getElementById("common").innerHTML = "page hostname : " + window.location.hostname
+
+// document.getElementById("common").innerHTML = "page url : " + window.location.assign
+// function goForward(){
+
+//     window.history.forward()
+// }
+
+// navigator 
+// document.getElementById("common").innerHTML = "Navigator features " + navigator.platform
+
+
+// window.alert("hello alret ")
+
+// window.confirm
+
+// function goconfirm(){
+//     var check; 
+
+//     if(prompt("hello you are confirmed")){
+//         check ="we are confirm"
+//     }else{
+//         check="we are not confirmed "
+//     }
+//     document.getElementById("common").innerHTML = check;
+// }
+
+
+// setInterval(()=>{
+//     console.log("hello set time ")
+// },3000)
+
+
+console.log(document.cookie = username=hari)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
